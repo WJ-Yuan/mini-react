1274e566d0426d6848ce8e5a07b875ee1dedb985
davidyuan <ywjdavid@126.com>
2024-12-18 21:25:11 +0800

    :sparkles: concurrent mode

--- react/render.js
+++ react/render.js
@@ -1,5 +1,21 @@
 import { TEXT_ELEMENT } from "./constant";
 
+let nextUnitOfWork = null;
+requestIdleCallback(workLoop);
+
+function workLoop(deadline) {
+	let shouldYield = false;
+
+	if (nextUnitOfWork && !shouldYield) {
+		nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+		shouldYield = deadline.timeRemain() < 1;
+	}
+
+	requestIdleCallback(workLoop);
+}
+
+function performUnitOfWork(nextUnitOfWork) {}
+
 export function render(element, container) {
 	const dom =
 		element.type === TEXT_ELEMENT
